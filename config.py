a834769c311351a048498b3bd5ab607cea65a70b
bbhcstech <bbhcstech@gmail.com>
2026-08-17 22:11:37 -0700

    agent tested with different pc successfully

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 SERVER_IP = "192.168.1.5"
-AGENT_KEY = "ZSF7cp5t85Bae1hVnlFdt4k8"
+AGENT_KEY = "po9PDXzg5wyb8GDc8JqvQtJy"
 
 API_BASE_URL = f"http://{SERVER_IP}:8000/api"
 WEBSOCKET_URL = f"ws://{SERVER_IP}:8080/app/gamezonekey"
